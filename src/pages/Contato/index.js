56968992d2aa06ca3fc91461f6a641de28f96e65
vgont <vgontijo34@gmail.com>
2023-09-08 22:34:06 -0300

    create Contato component

---
+++ src/pages/Contato/index.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Contato() {
+    return (
+      <div>
+        <h1>Pagina Contato</h1>
+      </div>
+    );
+  }
+  
+  export default Contato;
